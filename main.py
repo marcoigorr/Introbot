2682a2a614dafb125afcab84963887b7b79f9dbc
marcoigorr <petronio.mrc@gmail.com>
2022-11-12 00:17:42 +0100

    added command with author mention

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-client = discord.Client(intents=discord.Intents.default())
+client = discord.Client(intents=discord.Intents.all())
 
 def log(label):
     print(f'\n[+] ' + label)
@@ -15,13 +15,19 @@ async def on_ready():
     servers = ''
     for guild in client.guilds:
         servers += guild.name + "; "
+
     log(f'{client.user} is connected to these guilds -> {servers}', )
         
     log(f'{client.user} is connected to {guild.name}(id: {guild.id})')
 
 @client.event
-async def on_message():
-    if message.content.startswith('cambio?'):
-        await message.channel.send('Cagati addosso')
+async def on_message(message):
+    log(f'New message from {message.author} = {message.content}')
+
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('cambio?'):
+        await message.channel.send(f'<@{message.author.id}> Cagati addosso')
 
 client.run(os.getenv('DISCORD_TOKEN'))
