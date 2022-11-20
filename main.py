8b63ff25b0bef21bacfb82b3e0aec2bc980cd77b
marcoigorr <petronio.mrc@gmail.com>
2022-11-20 23:19:20 +0100

    Pycharm recoded everything

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-import discord
-import os
-from dotenv import load_dotenv
+import bot
 
-load_dotenv()
-client = discord.Client(intents=discord.Intents.all())
-
-def log(label):
-    print(f'\n[+] ' + label)
-
-@client.event
-async def on_ready():
-    log(f'{client.user} has connected to Discord!')
-
-    servers = ''
-    for guild in client.guilds:
-        servers += guild.name + "; "
-
-    log(f'{client.user} is connected to these guilds -> {servers}', )
-        
-    log(f'{client.user} is connected to {guild.name}(id: {guild.id})')
-
-@client.event
-async def on_message(message):
-    log(f'New message from {message.author} = {message.content}')
-
-    if message.author == client.user:
-        return
-
-    if message.content.lower().startswith('cambio?'):
-        await message.channel.send(f'<@{message.author.id}> Cagati addosso')
-
-client.run(os.getenv('DISCORD_TOKEN'))
+if __name__ == '__main__':
+    bot.run_discord_bot()
