e7d1eb4d11b19ef6a74118c0da90532b982b6d3c
marcoigorr <petronio.mrc@gmail.com>
2022-11-21 00:13:58 +0100

    added commands

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+import youtube_dl
 
 import responses
 
@@ -32,11 +34,16 @@ async def send_message(message):
 def run_discord_bot():
     load_dotenv()
     _token = os.getenv('DISCORD_TOKEN')
-    client = discord.Client(intents=discord.Intents.all())
+
+    intents = discord.Intents.all()
+    client = commands.Bot(command_prefix='$', intents=intents)
+
+    client.remove_command('help')
 
     @client.event
     async def on_ready():
         log(f'{client.user} has connected to Discord!')
+        await client.change_presence(status=discord.Status.online, activity=discord.Game(name="$help", type=3))
 
         servers = ''
         for guild in client.guilds:
@@ -58,5 +65,28 @@ def run_discord_bot():
         log(f'{username} said: \'{user_message}\' ({channel})')
 
         await send_message(message)
+
+        await client.process_commands(message)
+
+    @client.command()
+    async def hello(ctx):
+        await ctx.send('Ciao')
+
+    @client.command(pass_context=True)
+    async def play(ctx, url:str):
+        if ctx.author.voice:
+            channel = ctx.message.author.voice.channel
+            voice = await channel.connect()
+
+        else:
+            await ctx.send('Non sei connesso a nessun canale vocale.')
+
+    @client.command(pass_context=True)
+    async def leave(ctx):
+        if ctx.voice_client:
+            await ctx.guild.voice_client.disconnect()
+            await ctx.send('Ho lasciato il canale vocale')
+        else:
+            await ctx.send('Non sono in un canale vocale')
 
     client.run(_token)
--- responses.py
+++ responses.py
@@ -2,3 +2,6 @@
 def handle_response(message) -> str:
     if message.content.lower() == 'cambio?':
         return f'<@{message.author.id}> Cagati addosso'
+
+    if message.content == '$help':
+        return f'Lo sapevi che Emer é sempre sul pezzo?'
